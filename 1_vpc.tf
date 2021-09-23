4bb034c1d494cb5d93ace977e8af0f7523cccec8
Daniel <dinganadaniel@gmail.com>
2021-09-23 18:37:41 -0500

    took out nat gateway, private route table , private sg and private ec2

--- 1_vpc.tf
+++ 1_vpc.tf
@@ -17,25 +17,7 @@ resource "aws_internet_gateway" "IGW_TF" {
   depends_on = [aws_vpc.mainvpc]
 }
 
-resource "aws_eip" "EIP" {
-  vpc = true
-  tags = {
-    Name = "EIP"
-  }
 
-}
-
-# The aws_nat_gateway itself will be kept in one of the public subnets 
-resource "aws_nat_gateway" "NATGW" {
-  allocation_id = aws_eip.EIP.id
-  subnet_id     = aws_subnet.public_subnets[0].id
-
-  tags = {
-    Name = "NATGW"
-  }
-  depends_on = [aws_eip.EIP, aws_subnet.public_subnets]
-
-}
 
 #############################################################################################################
 ############################################# public_subnets ################################################
@@ -55,19 +37,3 @@ resource "aws_subnet" "public_subnets" {
   depends_on = [aws_vpc.mainvpc]
 }
 
-#############################################################################################################
-############################################# private_subnets ################################################
-#############################################################################################################
-resource "aws_subnet" "private_subnets" {
-
-  count             = length(var.private_subnet_cidr)
-  vpc_id            = aws_vpc.mainvpc.id
-  cidr_block        = element(var.private_subnet_cidr, count.index)
-  availability_zone = element(var.availability_zone, count.index)
-
-
-  tags = {
-    Name = "${element(var.private_subnet_names, count.index)}"
-  }
-  depends_on = [aws_vpc.mainvpc]
-}
--- 2_routetables.tf
+++ 2_routetables.tf
@@ -1,4 +1,6 @@
-
+#############################################################################################################
+############################################# PublicRouteTable ##############################################
+#############################################################################################################
 resource "aws_route_table" "PublicRouteTable" {
   vpc_id = aws_vpc.mainvpc.id
   route {
@@ -32,28 +34,7 @@ The special [*] symbol iterates over all of the elements of the list given to it
   depends_on     = [aws_subnet.public_subnets, aws_route_table.PublicRouteTable]
 }
 
-# we can use the one PrivateRouteTable for the two private subnets
-# we add a route in the PrivateRouteTable to the aws_nat_gateway
-# This way both private subnets can have access to the internet.
-# The aws_nat_gateway itself will be kept in one of the public subnets
-resource "aws_route_table" "PrivateRouteTable" {
-  vpc_id = aws_vpc.mainvpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.NATGW.id
-  }
-
-  tags = {
-    Name = "PrivateRouteTable"
-  }
-  depends_on = [aws_vpc.mainvpc, aws_nat_gateway.NATGW]
-}
-
-# we associate all three private subnets with the one PrivateRouteTable created above
-resource "aws_route_table_association" "privateroutetableassociation" {
-  count          = length(var.private_subnet_cidr)
-  subnet_id      = element(aws_subnet.private_subnets.*.id, count.index)
-  route_table_id = aws_route_table.PrivateRouteTable.id
-  depends_on     = [aws_subnet.private_subnets, aws_route_table.PrivateRouteTable]
-}
+#############################################################################################################
+############################################# end ##############################################
+#############################################################################################################
 
